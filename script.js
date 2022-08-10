a31b73134ca80a8ee27251b941d317ac6790c22a
Abdulhadhi1 <108865426+Abdulhadhi1@users.noreply.github.com>
2022-08-10 14:39:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-setTimeout(function () { 
-   document.getElementById("content1").innerText = 10; 
-   setTimeout(function () { 
-     document.getElementById("content1").innerText = 9; 
-     setTimeout(function () { 
-       document.getElementById("content1").innerText = 8; 
-       setTimeout(function () { 
-         document.getElementById("content1").innerText = 7; 
-         setTimeout(function () { 
-           document.getElementById("content1").innerText = 6; 
-           setTimeout(function () { 
-             document.getElementById("content1").innerText = 5; 
-             setTimeout(function () { 
-               document.getElementById("content1").innerText = 4; 
-               setTimeout(function () { 
-                 document.getElementById("content1").innerText = 3; 
-                 setTimeout(function () { 
-                   document.getElementById("content1").innerText = 2; 
-                   setTimeout(function () { 
-                     document.getElementById("content1").innerText = 1; 
-                     setTimeout(function () { 
-                       document.getElementById("content1").innerText = ""; 
-                       document.getElementById( 
-                         "content2" 
-                       ).innerText = "happy Independence day".toUpperCase(); 
-                     }, 1000); 
-                   }, 1000); 
-                 }, 1000); 
-               }, 1000); 
-             }, 1000); 
-           }, 1000); 
-         }, 1000); 
-       }, 1000); 
-     }, 1000); 
-   }, 1000); 
- }, 0);
+let timer =document.getElementById("countDown");
+
+let bigin=10;
+
+setTimeout(()=>{
+  timer.innerHTML=bigin;
+  bigin--;
+   setTimeout(()=>{
+    timer.innerHTML=bigin;
+    bigin--;
+    setTimeout(()=>{
+      timer.innerHTML=bigin;
+      bigin--;
+      setTimeout(()=>{
+        timer.innerHTML=bigin;
+        bigin--;
+        setTimeout(()=>{
+          timer.innerHTML=bigin;
+          bigin--;
+          setTimeout(()=>{
+            timer.innerHTML=bigin;
+            bigin--;
+            setTimeout(()=>{
+              timer.innerHTML=bigin;
+              bigin--;
+              setTimeout(()=>{
+                timer.innerHTML=bigin;
+                bigin--;
+                setTimeout(()=>{
+                  timer.innerHTML=bigin;
+                  bigin--;
+                  setTimeout(()=>{
+                    timer.innerHTML=bigin;
+                    bigin--;
+                    setTimeout(()=>{
+                      timer.innerHTML="happy independence day";
+                      timer.classList.add('text-danger')
+                    },1000)
+                  },1000)
+                },1000)
+              },1000) 
+            },1000)
+          },1000) 
+        },1000) 
+      },1000)
+    },1000) 
+  },1000)
+},1000)
