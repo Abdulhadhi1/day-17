dac1d4296f512429c660f2f2834963d63632ba0a
Abdulhadhi1 <108865426+Abdulhadhi1@users.noreply.github.com>
2022-08-10 14:02:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
-let timer = document.getElementById('countDown'); 
- let begin = 10; 
-  
- setTimeout(() => { 
-     timer.innerHTML = begin; 
-     begin--; 
-     setTimeout(() => { 
-         timer.innerHTML = begin; 
-         begin--; 
-         setTimeout(() => { 
-             timer.innerHTML = begin; 
-             begin--; 
-             setTimeout(() => { 
-                 timer.innerHTML = begin; 
-                 begin--; 
-                 setTimeout(() => { 
-                     timer.innerHTML = begin; 
-                     begin--; 
-                     setTimeout(() => { 
-                         timer.innerHTML = begin; 
-                         begin--; 
-                         setTimeout(() => { 
-                             timer.innerHTML = begin; 
-                             begin--; 
-                             setTimeout(() => { 
-                                 timer.innerHTML = begin; 
-                                 begin--; 
-                                 setTimeout(() => { 
-                                     timer.innerHTML = begin; 
-                                     begin--; 
-                                     setTimeout(() => { 
-                                         timer.innerHTML = begin; 
-                                         begin--; 
-                                         setTimeout(() => { 
-                                             timer.innerHTML = 'Happy Independence Day'; 
-                                             timer.classList.add('text-danger') 
-                                         }, 1000) 
-                                     }, 1000) 
-                                 }, 1000) 
-                             }, 1000) 
-                         }, 1000) 
-                     }, 1000) 
-                 }, 1000) 
-             }, 1000) 
-         }, 1000) 
-     }, 1000) 
- }, 1000)
+setTimeout(function () { 
+   document.getElementById("content1").innerText = 10; 
+   setTimeout(function () { 
+     document.getElementById("content1").innerText = 9; 
+     setTimeout(function () { 
+       document.getElementById("content1").innerText = 8; 
+       setTimeout(function () { 
+         document.getElementById("content1").innerText = 7; 
+         setTimeout(function () { 
+           document.getElementById("content1").innerText = 6; 
+           setTimeout(function () { 
+             document.getElementById("content1").innerText = 5; 
+             setTimeout(function () { 
+               document.getElementById("content1").innerText = 4; 
+               setTimeout(function () { 
+                 document.getElementById("content1").innerText = 3; 
+                 setTimeout(function () { 
+                   document.getElementById("content1").innerText = 2; 
+                   setTimeout(function () { 
+                     document.getElementById("content1").innerText = 1; 
+                     setTimeout(function () { 
+                       document.getElementById("content1").innerText = ""; 
+                       document.getElementById( 
+                         "content2" 
+                       ).innerText = "happy Independence day".toUpperCase(); 
+                     }, 1000); 
+                   }, 1000); 
+                 }, 1000); 
+               }, 1000); 
+             }, 1000); 
+           }, 1000); 
+         }, 1000); 
+       }, 1000); 
+     }, 1000); 
+   }, 1000); 
+ }, 0);
