f9b69ad03ed8233c784a7ca5bdc8b2b77547a1a4
Abdulhadhi1 <108865426+Abdulhadhi1@users.noreply.github.com>
2022-08-10 13:24:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-
-var timer = document.querySelector(".timer");
-
-var seconds = new Promise((tim,sec) => {
-  let Timer = setInterval(() => {
-    var runtime = parseInt(document.querySelector(".timer").innerText);
-    timer.innerText = runtime - 1;
-   
-    if (runtime === 0) {
-     clearInterval(Timer);
-     tim((timer.innerText = "Happy Independence day"));
-    }else{
-      sec("hi i am abdul")
-    }
-
-  }, 1000);
-});
-console.log(seconds);
+let timer = document.getElementById('countDown'); 
+ let begin = 10; 
+  
+ setTimeout(() => { 
+     timer.innerHTML = begin; 
+     begin--; 
+     setTimeout(() => { 
+         timer.innerHTML = begin; 
+         begin--; 
+         setTimeout(() => { 
+             timer.innerHTML = begin; 
+             begin--; 
+             setTimeout(() => { 
+                 timer.innerHTML = begin; 
+                 begin--; 
+                 setTimeout(() => { 
+                     timer.innerHTML = begin; 
+                     begin--; 
+                     setTimeout(() => { 
+                         timer.innerHTML = begin; 
+                         begin--; 
+                         setTimeout(() => { 
+                             timer.innerHTML = begin; 
+                             begin--; 
+                             setTimeout(() => { 
+                                 timer.innerHTML = begin; 
+                                 begin--; 
+                                 setTimeout(() => { 
+                                     timer.innerHTML = begin; 
+                                     begin--; 
+                                     setTimeout(() => { 
+                                         timer.innerHTML = begin; 
+                                         begin--; 
+                                         setTimeout(() => { 
+                                             timer.innerHTML = 'Happy Independence Day'; 
+                                             timer.classList.add('text-danger') 
+                                         }, 1000) 
+                                     }, 1000) 
+                                 }, 1000) 
+                             }, 1000) 
+                         }, 1000) 
+                     }, 1000) 
+                 }, 1000) 
+             }, 1000) 
+         }, 1000) 
+     }, 1000) 
+ }, 1000)
